69481de0ef420d6feab49a05bb8a216f34318769
michael <lolidunno@7clams.org>
2016-01-19 19:23:01 -0800

    Tests and fields for medication model

--- rxcalc/models.py
+++ rxcalc/models.py
@@ -2,6 +2,11 @@ from django.db import models
 
 
 class Medication(models.Model):
-    pass
+    name = models.CharField(max_length=140, unique=True, default='')
+    concentration = models.CharField(max_length=140, blank=True, default='')
+    desc = models.TextField(blank=True, default='')
+
+    def __str__(self):
+        return str(self.name)
 
 # TODO: create fields for rx name, dosage, description, move dosage calc to helper function
--- rxcalc/tests/test_models.py
+++ rxcalc/tests/test_models.py
@@ -1,7 +1,36 @@
 from django.test import TestCase
+from django.db import IntegrityError
 
 from rxcalc.models import Medication
 
 
 class MedicationModelTest(TestCase):
-    pass
+
+    def test_name_field(self):
+        Medication.objects.create(name='Tramadol')
+        med = Medication.objects.get(name='Tramadol')
+        self.assertEqual(med, Medication.objects.first())
+
+    def test_name_field_is_unique(self):
+        med = Medication.objects.create(name='Tramadol')
+
+        with self.assertRaises(IntegrityError):
+            med_dupe = Medication.objects.create(name='Tramadol')
+            med_dupe.full_clean()
+
+    def test_concentration_field_can_be_null(self):
+        med = Medication.objects.create(name='Tramadol')
+        med.full_clean()
+        # Should not raise
+
+    def test_description_field(self):
+        med = Medication.objects.create(name='Tramadol', desc='an opioid pain medication used to \
+                                                        treat moderate to moderately severe pain')
+        target_med = Medication.objects.get(desc='an opioid pain medication used to \
+                                                        treat moderate to moderately severe pain')
+        self.assertEqual(med, target_med)
+
+    def test_medication_string_rep(self):
+        med = Medication.objects.create(name='Tramadol')
+        self.assertEqual(str(med), 'Tramadol')
+
