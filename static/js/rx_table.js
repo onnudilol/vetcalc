2bdb172984f29c5c98e1a8c69868a0c8d87d715a
michael <lolidunno@7clams.org>
2016-02-12 14:07:29 -0800

    AJAX now displays form errors.

--- static/js/rx_table.js
+++ static/js/rx_table.js
@@ -6,7 +6,9 @@ $(document).ready(function() {
 
         $.get('/rxcalc/calc/', {weight:weight}, function(data) {
             var calc_output = $('<div />').append(data).find('#id_rx_table').html();
+            var errors = $('<div />').append(data).find('#id_rx_errors').html();
             $('#id_rx_table').html(calc_output);
+            $('#id_rx_errors').html(errors);
         });
     });
 });
