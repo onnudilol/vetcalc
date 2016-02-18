7f1b09b4c1d4f3c0b8f88284af31d5ce49ec57c2
michael <lolidunno@7clams.org>
2016-02-17 22:50:29 -0800

    Modified url in rx_table.js

--- static/js/rx_table.js
+++ static/js/rx_table.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
         event.preventDefault();
         var weight = $('#id_weight').val();
 
-        $.get('/rxcalc/calc/', {weight:weight}, function(data) {
+        $.get('/calc/injection/', {weight:weight}, function(data) {
             var calc_output = $('<div />').append(data).find('#id_rx_table').html();
             var errors = $('<div />').append(data).find('#id_rx_errors').html();
             $('#id_rx_table').html(calc_output);
