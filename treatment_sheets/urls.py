c75dd73106b495b504859073ed7347456b875bf8
michael <lolidunno@7clams.org>
2016-03-11 21:40:36 -0800

    Stub functions and urls for treatment sheet views

--- treatment_sheets/urls.py
+++ treatment_sheets/urls.py
@@ -3,5 +3,9 @@ from django.conf.urls import url
 from treatment_sheets import views
 
 urlpatterns = [
-    url('^$', views.treatment_sheet, name='tx_sheet'),
+    url('^$', views.treatment_sheets, name='tx_sheet'),
+    url('^new/$', views.new_tx_sheet, name='new_tx_sheet'),
+    url('^(\d+)/$', views.view_treatment_sheet, name='view_tx_sheet'),
+    url('^(\d+)/add$', views.add_item_tx_sheet, name='add_item_tx_sheet'),
+    url('^(\d+)/del$', views.del_item_tx_sheet, name='del_item_tx_sheet'),
 ]
--- treatment_sheets/views.py
+++ treatment_sheets/views.py
@@ -1,5 +1,30 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
+
+from treatment_sheets.forms import NewTxSheetForm
+from common.models import Prescription
 
 
-def treatment_sheet(request):
+def treatment_sheets(request):
+    pass
+
+
+@login_required()
+def view_treatment_sheet(request):
     return render(request, '404.html', {'navbar': 'tx_sheet'})
+
+
+@login_required()
+def new_tx_sheet(request):
+    pass
+
+
+@login_required()
+def add_item_tx_sheet(request):
+    pass
+
+
+@login_required()
+def del_item_tx_sheet(request):
+    pass
