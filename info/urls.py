87f04c3136ac6f73c02a5ceb6465adb2ba1c7088
michael <lolidunno@7clams.org>
2016-03-24 10:57:24 -0700

    Info view for prescriptions

--- info/urls.py
+++ info/urls.py
@@ -5,5 +5,6 @@ from info import views
 urlpatterns = [
     url(r'^$', views.info, name='info'),
     url(r'inj/(?P<slug>[\w\-]+)/$', views.info_inj, name='info_inj'),
-    url(r'cri/(?P<slug>[\w\-]+)/$', views.info_cri, name='info_cri')
+    url(r'cri/(?P<slug>[\w\-]+)/$', views.info_cri, name='info_cri'),
+    url(r'pre/(?P<slug>[\w\-]+)/$', views.info_pre)
 ]
--- info/views.py
+++ info/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
 
-from common.models import Injection, CRI
+from common.models import Injection, CRI, Prescription
 
 
 def info(request):
     inj = Injection.objects.all()
     cri = CRI.objects.all()
+    pre = Prescription.objects.all()
     return render(request, 'info/info.html', {'navbar': 'info',
                                               'inj': inj,
-                                              'cri': cri})
+                                              'cri': cri,
+                                              'pre': pre})
 
 
 def info_inj(request, slug):
@@ -21,3 +23,9 @@ def info_cri(request, slug):
     cri = CRI.objects.get(slug=slug)
     return render(request, 'info/rx.html', {'navbar': 'info',
                                             'rx': cri})
+
+
+def info_pre(request, slug):
+    pre = Prescription.objects.get(slug=slug)
+    return render(request, 'info/rx.html', {'navbar': 'info',
+                                            'rx': pre})
