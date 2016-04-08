ad2311d8109839622c109fbc03448d2bfe61151a
michael <lolidunno@7clams.org>
2016-04-08 13:37:03 -0700

    Updated txsheet FTs to current implementation

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -1,6 +1,5 @@
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from django.contrib.auth import get_user_model
-from django.contrib.auth.models import User
 
 from common.models import Injection, CRI, Prescription
 from treatment_sheets.models import TxSheet, TxItem
@@ -9,7 +8,7 @@ from selenium import webdriver
 
 import sys
 
-
+User = get_user_model()
 DEFAULT_WAIT = 10
 
 
@@ -33,15 +32,15 @@ class FunctionalTest(StaticLiveServerTestCase):
             super().tearDownClass()
 
     def setUp(self):
-        get_user_model().create_user(username='Marfalo', password='terriblepassword')
+        self.owner = User.objects.create_user('Marfalo', 'marfalo@gmail.com', 'terriblepw')
 
         Injection.objects.create(name='Tramadol', factor=1/50, concentration='5 mg/mL',
                                  category='Narcotic', admin='PO BID',
                                  desc='It can treat moderate to severe pain.')
         CRI.objects.create(name='Morphine', rates=[0.05, 0.005, 0.1, 0.001], factor=1/15, units="mg", calc_type='ez')
         CRI.objects.create(name='Dobutamine', factor=1/12500, calc_type='adv')
-        Prescription.objects.create(name='Tylenol', desc='Miracle drug that cures everything.')
-        Prescription.objects.create(name='Robitussin', desc='DMX')
+        self.scrip1 = Prescription.objects.create(name='Tylenol', desc='Miracle drug that cures everything.')
+        self.scrip2 = Prescription.objects.create(name='Robitussin', desc='DMX')
 
         profile = webdriver.FirefoxProfile()
         profile.set_preference("browser.startup.homepage", "about:blank")
@@ -57,11 +56,7 @@ class FunctionalTest(StaticLiveServerTestCase):
     def get_item_input_box(self):
         return self.browser.find_element_by_id('id_weight')
 
-    def login_user(self):
-        self.client.login(username='Marfalo', password='terriblepassword')
-
     def create_list(self):
-        marfalo = User.objects.get(username='Marfalo')
-        tx_sheet = TxSheet.objects.create(owner=marfalo, name='DD')
-        item1 = TxItem.objects.create(med=Injection.objects.get(name='Tylenol'))
-        tx_sheet.item_set.add(item1)
+        tx_sheet = TxSheet.objects.create(owner=self.owner, name='DD', comment='i heart diamond dogs')
+        item1 = TxItem.objects.create(sheet=tx_sheet, med=self.scrip1)
+        tx_sheet.txitem_set.add(item1)
--- functional_tests/test_tx_sheet.py
+++ functional_tests/test_tx_sheet.py
@@ -2,8 +2,17 @@ from .base import FunctionalTest
 
 from selenium.webdriver.common.keys import Keys
 
+import time
+
 
 class TreatmentSheetTest(FunctionalTest):
+
+    def login_user(self):
+        self.browser.get(self.server_url + '/tx_sheet/')
+        self.browser.find_element_by_link_text('log in').send_keys(Keys.RETURN)
+        self.browser.find_element_by_id('id_login').send_keys('Marfalo')
+        self.browser.find_element_by_id('id_password').send_keys('terriblepw')
+        self.browser.find_element_by_class_name('btn').send_keys(Keys.RETURN)
 
     def test_creating_new_treatment_sheet(self):
         # Dr. T is worried about liability so he is making the techs
@@ -11,7 +20,6 @@ class TreatmentSheetTest(FunctionalTest):
         # This is disruptive to their workflow and highly repetitive.
         # Marfalo checks VetCalc for solution to his ennui.
         # Fortuitously, the dev has implemented a treatment sheet creator.
-        self.browser.get(self.server_url + '/calc/tx_sheet/')
 
         # The page asks Marfalo to authenticate.  Marfalo goes through the process of registration, and is redirected
         # back to this page.  It says that he has not created any treatment sheets and invites him to do so.
@@ -21,32 +29,42 @@ class TreatmentSheetTest(FunctionalTest):
         # The page displays a form asking for a name, choice of medication, number of units per dose,
         # and frequency of dose.
         self.browser.find_element_by_id('id_name').send_keys('Partario')
-        self.browser.find_element_by_id('id_rx_choice').send_keys('Tylenol\n')
-        self.browser.find_element_by_id('id_dose_units').send_keys('5')
-        self.browser.find_element_by_id('id_dose_freq').send_keys('MLBID\n')
+        self.browser.find_element_by_id('id_comment').send_keys('Got neutered')
+        self.browser.find_element_by_id('id_med').send_keys('t')
+        self.browser.find_element_by_id('id_dose').send_keys('5')
+        self.browser.find_element_by_id('id_freq').send_keys('s')
+        self.browser.find_element_by_id('id_unit').send_keys('m')
         self.browser.find_element_by_class_name('btn').send_keys(Keys.RETURN)
 
         # Marfalo is redirected to a page that displays the current treatment sheet and all its items.
-        drugs = self.browser.find_elements_by_class_name('rx_item')
-        self.assertIn('Take 5 mL of Tylenol twice a day', drugs[0].text)
+        instructions = self.browser.find_element_by_class_name('content')
+        self.assertIn('Take 5.0 mLs of Tylenol once a day.', instructions.text)
 
     def test_edit_existing_list(self):
         self.login_user()
         self.create_list()
 
         # Marfalo wants to add another drug to the treatment sheet, so he clicks on the link to do so.
-        self.browser.find_element_by_link_text('Add another item').send_keys(Keys.RETURN)
+        self.browser.find_element_by_link_text('DD: i heart diamond dogs').send_keys(Keys.RETURN)
 
         # The page displays a form similar to the new list page.  Marfalo adds the item he needs.
-        self.browser.find_element_by_id('id_rx_choice').send_keys('Robitussin\n')
-        self.browser.find_element_by_id('id_dose_units').send_keys('3')
-        self.browser.find_element_by_id('id_dose_freq').send_keys('CSID\n')
+        self.browser.find_element_by_id('id_med').send_keys('r')
+        self.browser.find_element_by_id('id_dose').send_keys('3')
+        self.browser.find_element_by_id('id_freq').send_keys('s')
+        self.browser.find_element_by_id('id_unit').send_keys('c')
         self.browser.find_element_by_class_name('btn').send_keys(Keys.RETURN)
 
         # He is redirected to the list page, which now displays the item he just added.
-        drugs = self.browser.find_elements_by_class_name('rx_item')
-        self.assertIn('Take 3 capsules of Robitussin once a day', drugs[1].text)
+        instructions = self.browser.find_element_by_class_name('content')
+        self.assertIn('Take 3.0 capsules of Robitussin once a day', instructions.text)
 
         # Marfalo changes his mind and decides to delete the item he just added
-        removed_item = self.browser.find_elements_by_link_text('Remove item')[1].send_keys(Keys.RETURN)
-        self.assertNotIn('Robitussin', removed_item.text)
+        self.browser.get(self.server_url + '/tx_sheet/1/2/del')
+        time.sleep(5)
+        no_instructions = self.browser.find_element_by_class_name('content')
+        self.assertNotIn('Take 3.0 capsules of Robitussin once a day', no_instructions.text)
+
+        # Marfalo changes his mind again and decides to delete the whole list
+        self.browser.get(self.server_url + '/tx_sheet/1/del')
+        no_list = self.browser.find_element_by_class_name('content')
+        self.assertNotIn('DD: i heart diamond dogs', no_list.text)
